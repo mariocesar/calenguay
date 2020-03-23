0c71e45e83d76bca4cd1f6ec98cfc928fd762042
Mario César <mariocesar.c50@gmail.com>
2020-03-23 16:57:03 -0400

    Setup for deploy

--- calenguay/settings/base.py
+++ calenguay/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = '$*9caxzk)3ju3w+^j^4q-k6$1h%hl*)92f425f^lu4)@ylp4%6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "public/static"))
+
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, "public/media"))
